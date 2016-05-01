a4446ab02ca44dd26db78a58c01de56cc398e2b5
Enquinity <enquinity@gmail.com>
2016-05-01 19:47:49 +0200

    Improvements

--- minor.php
+++ minor.php
@@ -32,8 +32,29 @@ interface IDataStructure {
     public function getEntityStructure($entityName);
 }
 
-class DataToObjectMapper {
-    protected $cachedResults = [];
+class MapFieldsCalc {
+    public static function getMapFieldsFromColumnList(array $columnNames) {
+        $mapFields = [];
+        foreach ($columnNames as $columnName) {
+            $p = strrpos($columnName, '.');
+            if (false === $p) {
+                $mapFields[] = ['key' => $columnName, 'relation' => '.', 'fieldName' => $columnName];
+            } else {
+                $relation = substr($columnName, 0, $p);
+                $field = substr($columnName, $p + 1);
+                $mapFields[] = ['key' => $columnName, 'relation' => $relation, 'fieldName' => $field];
+            }
+        }
+        return $mapFields;
+    }
+}
+
+// todo: klasa bazowa plus 2 klasy pochodne - iterator i klasa do mapowania 1-dnego wiersza
+class DataToObjectsMapper implements \Iterator {
+    protected $segments = [];
+    protected $currentPos;
+    protected $currentSegment;
+    protected $currentSegmentPos;
 
     /**
      * @var IEntityActivator
@@ -51,21 +72,116 @@ class DataToObjectMapper {
     protected $sourceData;
     protected $sourceDataCount;
 
+    /**
+     * @var array
+     * [['key' => source data key, 'relation' => relation name (.) for main entity, 'fieldName' => name object field source data key is to be mapped to]]
+     */
+    protected $mapFields = [];
+
+    protected $mapInstructions = [];
+
     protected $startEntityName;
-    protected $extraRelations = [];
+    protected $extraRelations = null;
 
     protected $relPaths = null;
 
     protected $segmentSize = 100;
 
+    const ConversionNone = 0;
+    const ConversionToInt = 1;
+    const ConversionToFloat = 2;
+    const ConversionToBool = 3;
+
+    /**
+     * DataToObjectsMapper constructor.
+     * @param IDataStructure $dataStructure
+     * @param \Iterator      $sourceData
+     * @param                $sourceDataCount
+     * @param array          $mapFields
+     * @param                $startEntityName
+     * @param null           $extraRelations
+     */
+    public function __construct(IDataStructure $dataStructure, \Iterator $sourceData, $sourceDataCount, array $mapFields, $startEntityName, $extraRelations = null) {
+        $this->dataStructure = $dataStructure;
+        $this->sourceData = $sourceData;
+        $this->sourceDataCount = $sourceDataCount;
+        $this->mapFields = $mapFields;
+        $this->startEntityName = $startEntityName;
+        $this->extraRelations = $extraRelations;
+    }
+
+    public function current() {
+        if (!$this->valid()) {
+            return null;
+        }
+        return $this->segments[$this->currentSegment][$this->currentSegmentPos];
+    }
+
+    public function next() {
+        $this->currentPos++;
+        $this->currentSegmentPos++;
+        if ($this->currentSegmentPos >= count($this->segments[$this->currentSegment])) {
+            $this->currentSegmentPos = 0;
+            $this->currentSegment++;
+            $this->ensureCurrentSegmentFetched();
+        }
+    }
+
+    public function key() {
+        return $this->currentPos;
+    }
+
+    public function valid() {
+        return $this->currentPos < $this->sourceDataCount;
+    }
+
+    public function rewind() {
+        $this->currentPos = 0;
+        $this->currentSegment = 0;
+        $this->currentSegmentPos = 0;
+        $this->ensureCurrentSegmentFetched();
+    }
+
+    protected function ensureCurrentSegmentFetched() {
+        if (count($this->segments) <= $this->currentSegment) {
+            $this->fetchNextSegment();
+        }
+    }
+
+    protected function calcMapInstructions() {
+        foreach ($this->mapFields as $mapField) {
+            $this->mapInstructions[self::ConversionNone][] = [
+                $mapField['key'],
+                $this->relPaths[$mapField['relation']]['objectIdx'],
+                $mapField['fieldName'],
+            ];
+            // TODO: zbadać typ pola i do odpowiedniego indeksu wpisać
+        }
+    }
+
     protected function fetchNextSegment() {
         $toFetch = $this->segmentSize;
-        // TODO: trzeba sprawdzić, ile rekordów pozostało w źródle danych
+        $remaining = $this->sourceDataCount - $this->currentPos;
+        if (0 == $remaining) return;
+        if ($toFetch > $remaining) $toFetch = $remaining;
+
         $objects = $this->createObjects($toFetch);
         for ($toFetch = $this->segmentSize; $toFetch > 0 && $this->sourceData->valid(); $toFetch--, $this->sourceData->next()) {
             $row = $this->sourceData->current();
+            $objectNum = 0; // TODO: uzupełnić
 
+            foreach ($this->mapInstructions[self::ConversionNone] as $mapField) {
+                $field = $mapField[2];
+                $objects[$mapField[1]][$objectNum]->$field = $row[$mapField[0]];
+            }
+            // TODO: pozostałe konwersje
         }
+        $this->segments[] = $objects[$this->relPaths['.']['objectIdx']];
+        // TODO: zrobić to w bardziej wydajny sposób
+        /*foreach ($objects[$this->relPaths['.']['objectIdx']] as $o) {
+            $this->results[] = $o;
+        }*/
+        //$this->results = array_merge($this->results, $objects[$this->relPaths['.']['objectIdx']]);
     }
 
     protected function createObjects($count) {
@@ -111,9 +227,23 @@ class DataToObjectMapper {
             'parentPath' => null,
             'localPath' => $this->startEntityName,
         ];
-        foreach ($this->extraRelations as $relName) {
-            if (array_key_exists($relName, $this->relPaths)) continue;
-            $this->calcRelInfo($relName);
+        $extraRelations = $this->extraRelations;
+        if (null === $extraRelations) {
+            $er = [];
+            foreach ($this->mapFields as $mapField) {
+                if ($mapField['relation'] != '.') {
+                    $er[$mapField['relation']] = true;
+                }
+            }
+            if (!empty($er)) {
+                $extraRelations = array_keys($er);
+            }
+        }
+        if (null !== $extraRelations) {
+            foreach ($extraRelations as $relName) {
+                if (array_key_exists($relName, $this->relPaths)) continue;
+                $this->calcRelInfo($relName);
+            }
         }
     }
 }
